be60271c224550762105336a35cb3254c3c749f6
afkaf <62219143+afkaf@users.noreply.github.com>
2024-02-26 17:33:05 +0300

    Reduced code

--- streamcrypt.py
+++ streamcrypt.py
@@ -3,15 +3,12 @@ import secrets
 import time
 import numpy as np
 
-def hasher(data):
-    return blake3.blake3(data).digest()
-
 def generate_unique_salt():
     salt = secrets.token_bytes(32)
     unix_time = int(time.time_ns())
     time_bytes = unix_time.to_bytes((unix_time.bit_length() + 7) // 8, 'big')
     unique_input = salt + time_bytes
-    key = hasher(unique_input)
+    key = blake3.blake3(unique_input).digest()
     return key
 
 def generate_keystream(key, length):
@@ -21,7 +18,7 @@ def generate_keystream(key, length):
 
 def encrypt(keyword, data):
     salt = generate_unique_salt()  # salt generation
-    initial_key = hasher(keyword + salt)  # Combine and hash password and salt
+    initial_key = blake3.blake3(keyword + salt).digest()  # Combine and hash password and salt
     keystream = generate_keystream(initial_key, len(data))
     data_array = np.frombuffer(data, dtype=np.uint8)
     keystream_array = np.frombuffer(keystream, dtype=np.uint8)
@@ -30,7 +27,7 @@ def encrypt(keyword, data):
 
 def decrypt(keyword, secret):
     salt, encrypted_data = secret[:32], secret[32:]  # Extract salt and encrypted data
-    initial_key = hasher(keyword + salt)  # Recreate initial key
+    initial_key = blake3.blake3(keyword + salt).digest()  # Recreate initial key
     keystream = generate_keystream(initial_key, len(encrypted_data))
     encrypted_array = np.frombuffer(encrypted_data, dtype=np.uint8)
     keystream_array = np.frombuffer(keystream, dtype=np.uint8)
